3131209870cf4edd021c9bc5cd3dc6c6d9d4d4b1
olisettythanush-cell <olisettythanush@gmail.com>
2026-06-09 19:20:07 +0530

    Fix item registration and model loading for weapons

--- src/main/java/com/yourname/rivalsmod/util/RegistryHandler.java
+++ src/main/java/com/yourname/rivalsmod/util/RegistryHandler.java
@@ -2,7 +2,6 @@ package com.yourname.rivalsmod.util;
 
 import com.yourname.rivalsmod.init.ModWeapons;
 import net.minecraft.item.Item;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -10,35 +9,30 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = "rivalsmod")
 public class RegistryHandler {
 
-    // =========================
-    // ITEM REGISTRATION
-    // =========================
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event) {
 
         event.getRegistry().registerAll(
+                ModWeapons.BULLET,
                 ModWeapons.ASSAULT_RIFLE,
                 ModWeapons.EXOGUN,
-                ModWeapons.KATANA,
-                ModWeapons.BULLET
+                ModWeapons.KATANA
         );
     }
 
-    // =========================
-    // MODEL REGISTRATION
-    // =========================
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onModelRegister(ModelRegistryEvent event) {
 
+        registerModel(ModWeapons.BULLET);
         registerModel(ModWeapons.ASSAULT_RIFLE);
         registerModel(ModWeapons.EXOGUN);
         registerModel(ModWeapons.KATANA);
-        registerModel(ModWeapons.BULLET);
     }
 
     @SideOnly(Side.CLIENT)
